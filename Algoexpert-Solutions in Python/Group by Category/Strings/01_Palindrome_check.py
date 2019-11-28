5cb75535a36db57357a52d947680e1397b432038
raghunandepu <raghu.ndp@gmail.com>
2019-11-28 10:50:13 +0530

    Strings

--- Algoexpert-Solutions in Python/Group by Category/Strings/01_Palindrome_check.py
+++ Algoexpert-Solutions in Python/Group by Category/Strings/01_Palindrome_check.py
@@ -5,6 +5,7 @@ Write a function that takes in a non-empty string and that returns a boolean rep
 Sample input: "abcdcba"
 Sample output: True (it is a palindrome)"""
 
+# Solution 4: Best solution 
 # O(n) time | O(1) space
 def isPalindrome(string):
   start_index = 0
@@ -17,3 +18,38 @@ def isPalindrome(string):
   return True
 		
 print(isPalindrome("abccba"))
+
+#Other Solutions:
+
+# Solution 1: We create an empty string and add each character from last, then compare with original.
+
+# O(n**2) time due to traversal from last for n chars and adding them again to reversed string | O(n) space
+"""def isPalindrome(string):
+  reversedString = ""
+  
+  #Traversing through reversed index from last
+  for i in reversed(range(len(string))):
+    reversedString += string[i]
+  return reversedString == string
+print(isPalindrome("abccba"))"""
+
+# Solution 2:
+# We use array rather than creating a new string
+# O(n) time | O(n) space
+"""def isPalindrome(string):
+  reversedChars = []
+  for i in reversed(range(len(string))):
+    reversedChars.append(string[i])
+  return string == "".join(reversedChars)
+
+print(isPalindrome("abccba"))"""
+
+# Solution 3:
+# Using recursive way - Compare first and last character and also do recursive check on the middle string
+# O(n) time | O(n) space due to call stack (recursive)
+"""def isPalindrome(string, i= 0):
+  j = len(string) - 1 - i
+  return True if i >=j else string[i] == string[j] and isPalindrome(string, i + 1)
+
+print(isPalindrome("abccbaw"))"""
+
