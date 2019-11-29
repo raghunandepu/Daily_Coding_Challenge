24e1651060dc1e2f72dcba5da8414a1eb4ff287e
raghunandepu <raghu.ndp@gmail.com>
2019-11-29 23:26:27 +0530

    Strings

--- Algoexpert-Solutions in Python/Group by Category/Strings/05_Longest_substring_wo_duplication.py
+++ Algoexpert-Solutions in Python/Group by Category/Strings/05_Longest_substring_wo_duplication.py
@@ -1,8 +1,24 @@
-​
-"""Longest Substring Without Duplication
-​
-Write a function that takes in a string and that returns its longest substring without duplicate characters. Assume that there will only be one longest substring without duplication.
-​
+# Longest Substring Without Duplication (Hard)
+"""Write a function that takes in a string and that returns its longest substring without duplicate characters. Assume that there will only be one longest substring without duplication.
+
 Sample input: "clementisacap"
 Sample output: "mentisac"
 """
+
+def longestSubstring(string):
+  lastSeen = {}
+  longest = [0, 1]
+  startIdx = 0
+  
+  for i, char in enumerate(string):
+    if char in lastSeen:
+      startIdx = max(startIdx, lastSeen[char]+ 1)
+    if longest[1] - longest[0] < i + 1 - startIdx:
+      longest = [startIdx, i + 1]
+    lastSeen[char] = i
+  return string[longest[0]: longest[1]]
+
+string = "clementisacap"
+print(longestSubstring(string))
+      
+    
