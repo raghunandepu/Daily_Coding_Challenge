4d543d4f676127c895be04b9b660ab1419716ee1
raghunandepu <raghu.ndp@gmail.com>
2019-12-02 19:22:31 +0530

    Strings

--- Algoexpert-Solutions in Python/Group by Category/Strings/01_Palindrome_check.py
+++ Algoexpert-Solutions in Python/Group by Category/Strings/01_Palindrome_check.py
@@ -1,4 +1,4 @@
-"""Palindrome Check
+"""Palindrome Check (Easy)
 ​
 Write a function that takes in a non-empty string and that returns a boolean representing whether or not the string is a palindrome. A palindrome is defined as a string that is written the same forward and backward.
 ​
--- Algoexpert-Solutions in Python/Group by Category/Strings/03_LongestPalindromicSubstring.py
+++ Algoexpert-Solutions in Python/Group by Category/Strings/03_LongestPalindromicSubstring.py
@@ -1,4 +1,4 @@
-"""Longest Palindromic Substring
+"""Longest Palindromic Substring (Medium)
 ​
 Write a function that, given a string, returns its longest palindromic substring. A palindrome is defined as a string that is written the same forward and backward. Assume that there will only be one longest palindromic substring.
 ​
