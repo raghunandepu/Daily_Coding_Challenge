b7e67e400e4d5e647d83c286191355c92e869e56
raghunandepu <raghu.ndp@gmail.com>
2019-12-06 23:17:26 +0530

    Daily Challenge

---
+++ CodeForces/002_TheatreSquare.py
@@ -0,0 +1,26 @@
+'''
+Theatre Square in the capital city of Berland has a rectangular shape with the size n × m meters. On the occasion of the city's anniversary, a decision was taken to pave the Square with square granite flagstones. Each flagstone is of the size a × a.
+What is the least number of flagstones needed to pave the Square? It's allowed to cover the surface larger than the Theatre Square, but the Square has to be covered. It's not allowed to break the flagstones. The sides of flagstones should be parallel to the sides of the Square.
+Input
+The input contains three positive integer numbers in the first line: n,  m and a (1 ≤  n, m, a ≤ 109).
+Output
+Write the needed number of flagstones.
+'''
+
+class TheatreSquare:
+  def solve(self, n, m, a):
+    i = n//a # n/a flagsones to cover side length of n. // is floor division and discards fractional part.
+    if (n % a != 0):
+      i += 1
+    
+    j = m//a # m/a flagstones to cover another side of width m
+
+    if (m % a != 0):
+      j += 1
+      
+    return (i*j)
+  
+if __name__ == '__main__':
+  n,m,a = map(int, input().split())
+  ts = TheatreSquare()
+  print (ts.solve(n,m,a))
