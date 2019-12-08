0ae82a940c5d147ebbfa64b052bbc9f3f1aed73e
raghunandepu <raghu.ndp@gmail.com>
2019-12-08 11:16:52 +0530

    DC - Daily Challenge

--- SmartInterviews/002_array_sum.py
+++ SmartInterviews/002_array_sum.py
@@ -1,7 +1,11 @@
-n = int(input())
-
-
-
-total = sum([int(x) for x in input().split])
-
-print (total)
+#https://www.hackerrank.com/contests/smart-interviews/challenges/si-rotation-of-matrix
+t = int(input())
+for p in range(t):
+    print (f'Test Case #{p+1}:') 
+    n = int(input()) # size of the matrix. 2 stands for 2X2
+    a = [[int(x) for x in input().split()] for _x in range(n)] # elements of matrix as in input
+    a = [[str(a[j][i]) for j in range(n)] for i in range(n)] # swapping diagonal elements first
+    a = list(map(reversed, a)) # then reversing elements in each row
+    for row in a:
+           print(' '.join(row))
+           
