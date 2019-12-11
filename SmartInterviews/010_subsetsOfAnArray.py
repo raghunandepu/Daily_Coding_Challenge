8bde41f017a49fdb3a7a40aea24aac5c3ae13390
raghunandepu <raghu.ndp@gmail.com>
2019-12-11 11:08:14 +0530

    DC

--- SmartInterviews/010_subsetsOfAnArray.py
+++ SmartInterviews/010_subsetsOfAnArray.py
@@ -1,5 +1,61 @@
 from itertools import combinations
 
+# https://www.hackerrank.com/contests/smart-interviews/challenges/si-subsets-of-an-array
+"""Given an array of unique integer elements, print all the subsets of the given array in lexicographical order. 
+
+Input Format
+
+First line of input contains T - number of test cases. Its followed by 2T lines, the first line contains N - the size of the array and second line contains the elements of the array. 
+
+Constraints
+
+1 <= T <= 100
+1 <= N <= 10
+0 <= A[i] <= 100
+
+Output Format
+
+For each test case, print the subsets of the given array in lexicographical order, separated by new line. Print an extra newline between output of different test cases.
+
+Sample Input 0
+
+3
+3
+5 15 3 
+2
+57 96 
+4
+3 15 8 23 
+Sample Output 0
+
+3 
+3 5 
+3 5 15 
+3 15 
+5 
+5 15 
+15 
+
+57 
+57 96 
+96 
+
+3 
+3 8 
+3 8 15 
+3 8 15 23 
+3 8 23 
+3 15 
+3 15 23 
+3 23 
+8 
+8 15 
+8 15 23 
+8 23 
+15 
+15 23 
+23 """
+
 t = int(input())
 for tc in range(t):
   n = int(input())
---
+++ SmartInterviews/011_timeOfTheYear.py
@@ -0,0 +1,48 @@
+# https://www.hackerrank.com/contests/smart-interviews/challenges/si-time-of-the-year
+
+"""Given the number of seconds elapsed since epoch [01-01-1970 00:00:00 Thursday], you need to find the current date, along with the day.
+
+Input Format
+
+First line of input contains T - number of test cases. Its followed by T lines, each line contains the number of seconds elapsed since epoch. 
+
+Constraints
+
+1 <= T <= 10000 
+0 <= S <= 109
+
+Output Format
+
+For each test case, print the date in DD-MMM-YYYY format, followed by the day, separated by newline. 
+
+Sample Input 0
+
+10
+86399
+86400
+2592000
+2678400
+8639999
+8640000
+31535999
+31536000
+68169599
+68169600
+Sample Output 0
+
+01-JAN-1970 Thursday
+02-JAN-1970 Friday
+31-JAN-1970 Saturday
+01-FEB-1970 Sunday
+10-APR-1970 Friday
+11-APR-1970 Saturday
+31-DEC-1970 Thursday
+01-JAN-1971 Friday
+28-FEB-1972 Monday
+29-FEB-1972 Tuesday"""
+  
+import time
+
+t = int(input())
+for tc in range(t):
+    print(time.strftime("%d-%b-%Y %A", time.gmtime(int(input()))))
