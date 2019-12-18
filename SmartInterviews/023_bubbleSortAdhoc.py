7a9bc4bc2b9de36758129db02d7e8c29cc3c2f6a
raghunandepu <raghu.ndp@gmail.com>
2019-12-18 18:07:52 +0530

    DC

---
+++ SmartInterviews/023_bubbleSortAdhoc.py
@@ -0,0 +1,23 @@
+# Reference: algoexpert
+def bubbleSort(array):
+  isSorted = False
+  counter = 0
+  total_swaps = 0
+  while not isSorted:
+    isSorted = True
+    for i in range(len(array)-1):
+      if array[i] > array[i+1]:
+        total_swaps += 1
+        swap(i, i+1, array)
+        isSorted = False
+    counter += 1 # #This is to exclude already sorted numbers at the end
+  return total_swaps
+
+def swap(i, j, array):
+	array[i], array[j] = array[j], array[i]
+  
+t = int(input())
+for tc in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    print (bubbleSort(arr))
