87ffb78ec4f6222eeda9e4ef9fce4421a00d0a12
raghunandepu <raghu.ndp@gmail.com>
2020-01-08 22:29:03 +0530

    DC

---
+++ Algoexpert-Solutions in Python/Group by Category/Arrays/003_Smallest_difference.py
@@ -0,0 +1,38 @@
+# https://www.algoexpert.io/questions/Smallest%20Difference
+
+"""
+Smallest Difference
+​
+Write a function that takes in two non-empty arrays of integers. The function should find the pair of numbers (one from the first array, one from the second array) whose absolute difference is closest to zero. The function should return an array containing these two numbers, with the number from the first array in the first position. Assume that there will only be one pair of numbers with the smallest difference.
+​
+Sample input: [-1, 5, 10, 20, 28, 3], [26, 134, 135, 15, 17]
+Sample output: [28, 26]"""
+
+# O(nlog(n) + mlog(m)) time | O(1) space
+# for first array sorting + for second array sorting time
+
+def smallestDifference(arrayOne, arrayTwo):
+    # Write your code here.
+	arrayOne.sort()
+	arrayTwo.sort()
+	
+	idxOne = 0
+	idxTwo = 0
+	smallest = float("inf")
+	current = float("inf")
+	smallestPair = []
+	while idxOne < len(arrayOne) and idxTwo < len(arrayTwo):
+		firstNum = arrayOne[idxOne]
+		secondNum = arrayTwo[idxTwo]
+		if firstNum < secondNum:
+			current = secondNum - firstNum
+			idxOne += 1
+		elif secondNum < firstNum:
+			current = firstNum - secondNum
+			idxTwo += 1
+		else:
+			return [firstNum, secondNum]
+		if smallest > current:
+			smallest = current
+			smallestPair = [firstNum, secondNum]
+	return smallestPair
