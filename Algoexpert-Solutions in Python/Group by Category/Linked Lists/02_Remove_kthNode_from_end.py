0ff6be2620153be52a50002906987e287fa2a3bf
raghunandepu <raghu.ndp@gmail.com>
2020-01-22 17:12:02 +0530

    Daily Challenge

--- Algoexpert-Solutions in Python/Group by Category/Linked Lists/02_Remove_kthNode_from_end.py
+++ Algoexpert-Solutions in Python/Group by Category/Linked Lists/02_Remove_kthNode_from_end.py
@@ -1,3 +1,4 @@
+# https://www.algoexpert.io/questions/Remove%20Kth%20Node%20From%20End
 """
 Remove Kth Node From End
 ​
@@ -7,4 +8,36 @@ Sample input: 0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9, 4
 Sample output: 0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 7 -> 8 -> 9
 ​
 """
+# O(N), O(1)
+# where N is the number of nodes
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+def removeNthNodeFromEnd(head, n):
+  counter = 1
+  first = head
+  second = head
+  while counter <= n:
+    second = second.next
+    counter += 1
+  if second is None:
+    # it means we need to remove the first node which is at nth position from last
+    head.value = head.next.value
+    head.next = head.next.next
+    return
+    
+  # here we are checking second.next not second
+  while second.next is not None:
+    #first is pointing to the node right before the one we want to delete
+    first = first.next
+    second = second.next
+  first.next = first.next.next 
+  
+  
+  
+    
+    
+    
 
