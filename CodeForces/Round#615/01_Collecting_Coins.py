68686ade93e073f308fb31957e88c83e4bd7f937
raghunandepu <raghu.ndp@gmail.com>
2020-01-23 10:50:21 +0530

    Daily Challenge

---
+++ CodeForces/Round#615/01_Collecting_Coins.py
@@ -0,0 +1,112 @@
+# https://codeforces.com/contest/1294/problem/A
+"""
+A. Collecting Coins
+time limit per test2 seconds
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output
+Polycarp has three sisters: Alice, Barbara, and Cerene. They're collecting coins. Currently, Alice has 𝑎
+a
+ coins, Barbara has 𝑏
+b
+ coins and Cerene has 𝑐
+c
+ coins. Recently Polycarp has returned from the trip around the world and brought 𝑛
+n
+ coins.
+
+He wants to distribute all these 𝑛
+n
+ coins between his sisters in such a way that the number of coins Alice has is equal to the number of coins Barbara has and is equal to the number of coins Cerene has. In other words, if Polycarp gives 𝐴
+A
+ coins to Alice, 𝐵
+B
+ coins to Barbara and 𝐶
+C
+ coins to Cerene (𝐴+𝐵+𝐶=𝑛
+A
++
+B
++
+C
+=
+n
+), then 𝑎+𝐴=𝑏+𝐵=𝑐+𝐶
+a
++
+A
+=
+b
++
+B
+=
+c
++
+C
+.
+
+Your task is to find out if it is possible to distribute all 𝑛
+n
+ coins between sisters in a way described above.
+
+You have to answer 𝑡
+t
+ independent test cases.
+
+Input
+The first line of the input contains one integer 𝑡
+t
+ (1≤𝑡≤104
+1
+≤
+t
+≤
+10
+4
+) — the number of test cases.
+
+The next 𝑡
+t
+ lines describe test cases. Each test case is given on a new line and consists of four space-separated integers 𝑎,𝑏,𝑐
+a
+,
+b
+,
+c
+ and 𝑛
+n
+ (1≤𝑎,𝑏,𝑐,𝑛≤108
+1
+≤
+a
+,
+b
+,
+c
+,
+n
+≤
+10
+8
+) — the number of coins Alice has, the number of coins Barbara has, the number of coins Cerene has and the number of coins Polycarp has.
+
+Output
+For each test case, print "YES" if Polycarp can distribute all 𝑛
+n
+ coins between his sisters and "NO" otherwise."""
+ 
+ 
+class Collecting_Coins:
+  def solve(self, a, b, c, n):
+    result = (a + b + c + n) % 3
+    if result == 0:
+      return "YES"
+    return "NO"    
+  
+if __name__ == '__main__':
+  n = int(input())
+  for i in range(n):
+    a, b, c, n = map(int, input().split())
+    collectCoins = Collecting_Coins()
+    result = collectCoins.solve(a, b, c, n)
+    print (result)
---
+++ InterviewBit/023_Palindrome_List(linkedList).py
@@ -0,0 +1,175 @@
+# https://www.interviewbit.com/problems/palindrome-list/
+# Asked in Amazon, Microsoft
+
+"""
+Given a singly linked list, determine if its a palindrome. Return 1 or 0 denoting if its a palindrome or not, respectively.
+
+Notes:
+
+Expected solution is linear in time and constant in space.
+For example,
+
+List 1-->2-->1 is a palindrome.
+List 1-->2-->3 is not a palindrome."""
+
+import sys
+class node:
+  def __init__(self, data):
+    self.data = data
+    self.next = None
+
+class LinkedList:
+  def __init__(self):
+    self.head = None
+  
+  def display(self):
+    temp = self.head
+    while temp:
+      print(temp.data)
+      temp = temp.next
+  
+  def count(self):
+    counter = 0
+    temp = self.head
+    while temp:
+      counter += 1
+      temp = temp.next
+    return counter
+  
+  def search(self, value):
+    temp = self.head
+    pos = 1
+    while temp:
+      if temp.data == value:
+        print(The value found at", pos)
+      temp = temp.next
+      pos += 1
+    print("Value not found in the linked list")
+    
+  def insert_at_beginning(self, data):
+    self.temp = node(data)
+    if self.head is None:
+      self.head = self.temp
+      return
+    self.temp.next = self.head
+    self.head = temp
+    
+  def insert_at_end(self, data):
+    self.temp = node(data)
+    if self.head is None:
+      self.head = self.temp
+      return
+    self.p = self.head
+    while self.p.next is not None:
+      self.p = self.p.next
+    self.p.next = self.temp
+    
+  def insert_after_given_node(self, data, item):
+    self.p = self.head
+    while self.p is not None:
+      if self.p.data == item:
+        self.temp = node(data)
+        self.temp.next = self.p.next
+        self.p.next = self.temp
+        return
+      self.p = self.p.next
+    print ("Item not found")
+  
+  def insert_at_pos(self, data, pos):
+    self.temp = node(data)
+    if pos == 1:
+      self.temp.next = self.head
+      self.head = self.temp
+    self.p = self.head
+    while pos > 2 and self.p is not None:
+      self.p = self.p.next
+      pos -= 1
+    if self.p is None:
+      print ("Position exceeded the length of the list")
+    else:
+      self.temp.next = self.p.next
+      self.p.next = self.temp
+      
+  def delete(self, data):
+    if self.head is None:
+      print ("list is empty")
+      return
+    if self.head.info == data:
+      self.temp = self.head
+      self.head = self.head.next
+      return
+    self.p = None
+    self.curr = self.head
+    while self.curr is not None:
+      if self.curr.info == data:
+        self.temp = self.p.next
+        self.p.next = self.temp.next
+        return
+        
+      self.p = self.curr
+      self.curr = self.curr.next
+      
+  def reverse(head):
+    prev = None
+    curr = head
+    while curr is not None:
+      Next = curr.next 
+      curr.next = prev
+      prev = curr
+      curr = Next
+    head = prev
+    return head
+  
+  def compareList(head1, head2):
+    while head1 is not None and head2 is not None:
+      if head1.data = head2.data:
+        head1 = head1.next
+        head2 = head2.next
+      else:
+        return False
+      if head1 is None and head2 is None:
+        return True
+      return False
+    
+  
+  def isPalindrome(llist):
+    head = llist.head
+    slowptr = head
+    fastptr = head
+    slow_prev = None
+    nextHalf = None
+    midnode = None
+    
+    if head is not None and head.next is not None:
+      while fastptr is not None and fastptr.next is not None:
+        fastptr = fastptr.next.next 
+        slow_prev = slowptr # saving slow prev pointer also here
+        slowptr = slowptr.next
+      
+      if fastptr is Not None:
+        # it means we are checking for odd length palindrome
+        midnode = slowptr
+        slowptr = slowptr.next # next half starting
+      
+      nextHalf = slowptr
+      slow_prev.next = None
+      nextHalf = reverse(nextHalf) # using reverse function created above
+      result = compareList(head, nextHalf)
+      nextHalf = reverse(nextHalf) # reverting back the reversed second half
+      
+      # Now we need to join back the second half with first half.
+      if midnode is not None:
+        slow_prev.next = midnode
+        midnode.next = nextHalf
+      else:
+        slow_prev.next = nextHalf
+    return result
+        
+        
+        
+        
+        
+        
+  
+      
+    
